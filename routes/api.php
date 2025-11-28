ae16af56286fdd5db2ace4dcad24689186863783
ASADAMJAD922 <asadamjad922@gmail.com>
2025-11-28 23:28:54 +0500

    first commit

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\DeviceController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/test', function () {
+    return ['message' => 'API is working!'];
+});
+
+Route::middleware('device')->group(function () {
+
+    // Auth Controller
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/login-confirm', [AuthController::class, 'verifyOtp']);
+    Route::post('/login-resend', [AuthController::class, 'resendCode']);
+    Route::post('/register', [AuthController::class, 'register']);
+    
+    // Device Controller
+    Route::post('/device/web-sync', [DeviceController::class, 'syncDeviceWeb']);
+});
+
+// Post authenticated route
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
