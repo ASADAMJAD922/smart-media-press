ff128ca3e40ad642f48eff9592404e07816da75e
ASADAMJAD922 <asadamjad922@gmail.com>
2025-11-29 23:14:21 +0500

    fix in device middleware

--- app/Http/Middleware/DeviceApiMiddleware.php
+++ app/Http/Middleware/DeviceApiMiddleware.php
@@ -6,7 +6,6 @@ use App\Models\Device;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class DeviceApiMiddleware
@@ -18,85 +17,85 @@ class DeviceApiMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if ($request->header('Accept-Language')) {
-            App::setLocale($request->header('Accept-Language'));
-        }
-        if ($request->header('X-Device-Id')) {
+        // if ($request->header('Accept-Language')) {
+        //     App::setLocale($request->header('Accept-Language'));
+        // }
+        // if ($request->header('X-Device-Id')) {
 
-            $device = Device::query()->firstOrCreate(
-                [
-                    'device_id' => $request->header('X-Device-Id'),
-                    'is_mobile' => true,
-                ],
-                [
-                    'app_version' => $request->header('X-App-Version') ?? null,
-                    'device_token' => $request->header('X-DeviceToken') ?? null,
-                ]
-            );
+        //     $device = Device::query()->firstOrCreate(
+        //         [
+        //             'device_id' => $request->header('X-Device-Id'),
+        //             'is_mobile' => true,
+        //         ],
+        //         [
+        //             'app_version' => $request->header('X-App-Version') ?? null,
+        //             'device_token' => $request->header('X-DeviceToken') ?? null,
+        //         ]
+        //     );
 
-            $request->merge(['device' => $device]);
+        //     $request->merge(['device' => $device]);
 
-            if ($request->user('sanctum')) {
+        //     if ($request->user('sanctum')) {
 
-                $device->users()->syncWithoutDetaching($request->user('sanctum')->id);
-                $user = $request->user('sanctum');
-                $user->device_id = $device->device_id;
-                $user->device_token = $device->device_token;
-                $user->app_version = $device->app_version;
-                $user->device_manufacturer = $device->device_manufacturer;
-                $user->device_type = $device->device_type;
-                $user->save();
+        //         $device->users()->syncWithoutDetaching($request->user('sanctum')->id);
+        //         $user = $request->user('sanctum');
+        //         $user->device_id = $device->device_id;
+        //         $user->device_token = $device->device_token;
+        //         $user->app_version = $device->app_version;
+        //         $user->device_manufacturer = $device->device_manufacturer;
+        //         $user->device_type = $device->device_type;
+        //         $user->save();
 
-            }
-        } elseif ($request->header('X-Fingerprint')) {
+        //     }
+        // } elseif ($request->header('X-Fingerprint')) {
 
-            $device = Device::where('fingerprint_id', $request->header('X-Fingerprint'))->first();
-                if (!$device) {
-                    $device = Device::create([
-                        'fingerprint_id' => $request->header('X-Fingerprint'),
-                    ]);
-                }
-            $request->merge(['device' => $device]);
+        //     $device = Device::where('fingerprint_id', $request->header('X-Fingerprint'))->first();
+        //         if (!$device) {
+        //             $device = Device::create([
+        //                 'fingerprint_id' => $request->header('X-Fingerprint'),
+        //             ]);
+        //         }
+        //     $request->merge(['device' => $device]);
 
-            if ($request->user('sanctum')) {
+        //     if ($request->user('sanctum')) {
 
-                $device->users()->syncWithoutDetaching($request->user('sanctum')->id);
-                $user = $request->user('sanctum');
-                $user->device_id = $device->device_id;
-                $user->device_token = $device->device_token;
-                $user->app_version = $device->app_version;
-                $user->device_manufacturer = $device->device_manufacturer;
-                $user->device_type = $device->device_type;
-                $user->save();
+        //         $device->users()->syncWithoutDetaching($request->user('sanctum')->id);
+        //         $user = $request->user('sanctum');
+        //         $user->device_id = $device->device_id;
+        //         $user->device_token = $device->device_token;
+        //         $user->app_version = $device->app_version;
+        //         $user->device_manufacturer = $device->device_manufacturer;
+        //         $user->device_type = $device->device_type;
+        //         $user->save();
 
-            }
-        } else {
-            $device = Device::firstOrCreate(
-                [
-                    'device_id' => 'UNKNOWNDEVICEID',
-                    'is_mobile' => true,
-                ],
-                [
-                    'app_version' => $request->header('X-App-Version') ?? null,
-                ]
-            );
+        //     }
+        // } else {
+        //     $device = Device::firstOrCreate(
+        //         [
+        //             'device_id' => 'UNKNOWNDEVICEID',
+        //             'is_mobile' => true,
+        //         ],
+        //         [
+        //             'app_version' => $request->header('X-App-Version') ?? null,
+        //         ]
+        //     );
 
-            $request->merge(['device' => $device]);
+        //     $request->merge(['device' => $device]);
 
-            if ($request->user('sanctum')) {
-                $device->users()->syncWithoutDetaching($request->user('sanctum')->id);
-                $user = $request->user('sanctum');
+        //     if ($request->user('sanctum')) {
+        //         $device->users()->syncWithoutDetaching($request->user('sanctum')->id);
+        //         $user = $request->user('sanctum');
 
-                $user->device_id = $device->device_id;
-                $user->device_token = $device->device_token;
-                $user->app_version = $device->app_version;
-                $user->device_manufacturer = $device->device_manufacturer;
-                $user->device_type = $device->device_type;
-                $user->save();
+        //         $user->device_id = $device->device_id;
+        //         $user->device_token = $device->device_token;
+        //         $user->app_version = $device->app_version;
+        //         $user->device_manufacturer = $device->device_manufacturer;
+        //         $user->device_type = $device->device_type;
+        //         $user->save();
 
-            }
+        //     }
 
-        }
+        // }
         return $next($request);
 
     }
