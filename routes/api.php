b4b0173e15283854cbd097102c02ff357616b8d4
ASADAMJAD922 <asadamjad922@gmail.com>
2025-11-30 00:18:41 +0500

    routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\DeviceController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/testing', function () {
@@ -11,20 +10,19 @@ Route::get('/testing', function () {
 
 
 Route::middleware('device')->group(function () {
-
     // Auth Controller
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/login-confirm', [AuthController::class, 'verifyOtp']);
-    Route::post('/login-resend', [AuthController::class, 'resendCode']);
     Route::post('/register', [AuthController::class, 'register']);
-    
+    Route::post('/login', [AuthController::class, 'login']);
+
     // Device Controller
     Route::post('/device/web-sync', [DeviceController::class, 'syncDeviceWeb']);
 });
 
 // Post authenticated route
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/update-profile', [AuthController::class, 'updateProfile']);
+    Route::delete('/delete-account', [AuthController::class, 'deleteAccount']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
