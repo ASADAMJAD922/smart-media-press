537b0894bd5a0e7a5301cecb148a229974846394
ASADAMJAD922 <asadamjad922@gmail.com>
2025-12-02 00:58:27 +0500

    migrations

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\AuthorsController;
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\DeviceController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/testing', function () {
@@ -25,4 +28,33 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/update-profile', [AuthController::class, 'updateProfile']);
     Route::delete('/delete-account', [AuthController::class, 'deleteAccount']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    // ==========================
+    // PRODUCT CRUD
+    // ==========================
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::get('/products/{id}', [ProductController::class, 'show']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+
+    // ==========================
+    // CATEGORY CRUD
+    // ==========================
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::get('/categories/{id}', [CategoryController::class, 'show']);
+    Route::put('/categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
+
+    // ==========================
+    // AUTHOR CRUD 
+    // ==========================
+    Route::get('/authors', [AuthorsController::class, 'index']);
+    Route::post('/authors', [AuthorsController::class, 'store']);
+    Route::get('/authors/{id}', [AuthorsController::class, 'show']);
+    Route::put('/authors/{id}', [AuthorsController::class, 'update']);
+    Route::delete('/authors/{id}', [AuthorsController::class, 'destroy']);
 });
