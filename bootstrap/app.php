29527243103d5f7ccff9a0645cfd5c5081f49fb2
ASADAMJAD922 <asadamjad922@gmail.com>
2025-12-09 23:56:51 +0500

    fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -36,10 +36,13 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (NotFoundHttpException $e, Request $request) {
-            if ($request->is('api/*')) {
+            if (request()->is('api/*')) {
                 return response()->json([
                     'message' => 'Record not found.',
                 ], 404);
             }
+
+            return url('/');
+
         });
     })->create();
