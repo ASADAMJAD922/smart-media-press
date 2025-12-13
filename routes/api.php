fd4086852709657263b9c7b1b17d4337ab02c92d
ASADAMJAD922 <asadamjad922@gmail.com>
2025-12-13 18:22:17 +0500

    also create some api for website

--- routes/api.php
+++ routes/api.php
@@ -1,80 +1,67 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\AuthController;
-use App\Http\Controllers\Api\AuthorsController;
-use App\Http\Controllers\Api\CategoryController;
-use App\Http\Controllers\Api\DeviceController;
 use App\Http\Controllers\Api\ProductController;
-use App\Http\Controllers\Api\QuestionController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\AuthorsController;
 use App\Http\Controllers\Api\BlogController;
+use App\Http\Controllers\Api\QuestionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/testing', function () {
-    return ['message' => 'API is working!'];
-});
+/*
+|--------------------------------------------------------------------------
+| ADMIN AUTH (LOGIN / REGISTER)
+|--------------------------------------------------------------------------
+*/
 
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
-Route::middleware('device')->group(function () {
-    // Auth Controller
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-    // Device Controller
-    Route::post('/device/web-sync', [DeviceController::class, 'syncDeviceWeb']);
-});
+/*
+|--------------------------------------------------------------------------
+| ADMIN DASHBOARD ROUTES
+|--------------------------------------------------------------------------
+| auth:sanctum middleware required
+*/
+Route::prefix('admin')->middleware('auth:sanctum')->group(function () {
 
-// Post authenticated route
-Route::middleware('auth:sanctum')->group(function () {
-
-    // ==========================
-    // USER API'S
-    // ==========================
+    Route::get('/testing', function () {
+        return ['message' => 'API is working!'];
+    });
+    // User
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/update-profile', [AuthController::class, 'updateProfile']);
     Route::delete('/delete-account', [AuthController::class, 'deleteAccount']);
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    // ==========================
-    // PRODUCT CRUD
-    // ==========================
+    // Product CRUD
     Route::get('/products', [ProductController::class, 'index']);
     Route::post('/products', [ProductController::class, 'store']);
     Route::get('/products/{id}', [ProductController::class, 'show']);
     Route::put('/products/{id}', [ProductController::class, 'update']);
     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 
-
-    // ==========================
-    // CATEGORY CRUD
-    // ==========================
+    // Category CRUD
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::post('/categories', [CategoryController::class, 'store']);
     Route::get('/categories/{id}', [CategoryController::class, 'show']);
     Route::put('/categories/{id}', [CategoryController::class, 'update']);
     Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
 
-
-    // ==========================
-    // AUTHOR CRUD 
-    // ==========================
+    // Author CRUD
     Route::get('/authors', [AuthorsController::class, 'index']);
     Route::post('/authors', [AuthorsController::class, 'store']);
     Route::get('/authors/{id}', [AuthorsController::class, 'show']);
     Route::put('/authors/{id}', [AuthorsController::class, 'update']);
     Route::delete('/authors/{id}', [AuthorsController::class, 'destroy']);
-    
-    
-    // ==========================
+
     // Question CRUD
-    // ==========================
     Route::get('/questions', [QuestionController::class, 'index']);
     Route::post('/questions', [QuestionController::class, 'store']);
     Route::get('/questions/{question}', [QuestionController::class, 'show']);
     Route::post('/questions/{question}', [QuestionController::class, 'update']);
     Route::delete('/questions/{question}', [QuestionController::class, 'destroy']);
 
-    
-    // ==========================
     // Blog CRUD
-    // ==========================
-    Route::apiResource('blogs', BlogController::class);
+    Route::apiResource('/blogs', BlogController::class);
 });
