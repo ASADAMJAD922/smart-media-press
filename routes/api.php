3b40d058601a1dcafa07bfbe9bbab9ad06cfeecd
ASADAMJAD922 <asadamjad922@gmail.com>
2026-01-17 17:22:16 +0500

    routes

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,7 @@ Route::prefix('admin')->middleware('auth:sanctum')->group(function () {
     // Product CRUD
     Route::get('/products', [ProductController::class, 'index']);
     Route::post('/products', [ProductController::class, 'store']);
-    Route::get('/products/{id}', [ProductController::class, 'show']);
+    Route::get('/products/{slug}', [ProductController::class, 'show']);
     Route::put('/products/{id}', [ProductController::class, 'update']);
     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 
